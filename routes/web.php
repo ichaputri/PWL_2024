85cde0d80dd6c8b18fd20c38207e3cd07f7fa129
ichaputri <ichaputriana39@gmail.com>
2024-02-22 15:14:39 +0700

    Praktikum Routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+// Basic Router
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'Nim : 2241720069' . '<br>' . 'Nama : Icha Dewi Putriana';
+});
+
+
+
+// Route Parameters
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+
+
+// Optional Parameters
+Route::get('/user/{name?}', function ($name = ' ') {
+    return 'Nama saya ' . $name;
 });
